1c0f502e99bafc95900f6d0c42dfb6c364430a7c
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-04-19 15:55:38 +0100

    Run more servers as part of the test server
    And increase the body size of the content they send (now randomly chosen between
    8KiB and 72KiB).

--- command/testserver/testserver.go
+++ command/testserver/testserver.go
@@ -2,25 +2,60 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"math/rand"
 	"net/http"
+	"runtime"
+	"sync"
+	"time"
 )
 
 var quit = make(chan int)
 
+type randomDataMaker struct {
+	sync.Mutex
+	src rand.Source
+}
+
+// Read will fill the byte slice with random ASCII bytes
+func (r *randomDataMaker) Read(p []byte) (n int, err error) {
+	for i := range p {
+		p[i] = byte(r.src.Int63() & 0x7f) // 0x7f ensures the result is ASCII
+	}
+	return len(p), nil
+}
+
 func makeDebugServer(name string) http.HandlerFunc {
+	var randSrc = &randomDataMaker{src: rand.NewSource(time.Now().UTC().UnixNano())}
+
 	return func(w http.ResponseWriter, r *http.Request) {
+		randSrc.Lock()
+		defer randSrc.Unlock()
+
+		w.Header().Set("Content-Type", "text/plain")
+
 		fmt.Fprintln(w, name, ":", r.URL.Path)
+		fmt.Fprintln(w)
+
+		// pick a size between 8 and 72KiB
+		sz := 8192 + rand.Intn(1024*64)
+		io.CopyN(w, randSrc, int64(sz))
 	}
 }
 
 func main() {
-	debug1 := http.NewServeMux()
-	debug1.HandleFunc("/", makeDebugServer("whitehall"))
-	debug2 := http.NewServeMux()
-	debug2.HandleFunc("/", makeDebugServer("frontend"))
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	go http.ListenAndServe(":6061", debug1)
-	go http.ListenAndServe(":6062", debug2)
+	startPort := 6061
+	servers := []string{"one", "two", "three", "four", "five", "six"}
+
+	for i, s := range servers {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", makeDebugServer(s))
+		addr := fmt.Sprintf(":%d", startPort+i)
+		go http.ListenAndServe(addr, mux)
+		fmt.Println("Server", s, "listening on", addr)
+	}
 
 	<-quit
 }
