4f21e289c16aed3805509f5757a5ef10f759d1c1
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-10-04 14:47:24 +0000

    Extract compilation into separate script

--- jenkins.sh
+++ jenkins.sh
@@ -2,21 +2,10 @@
 set -x
 set -eu
 
-export GOPATH="$(pwd)/gopath.tmp"
-export GOBIN="$GOPATH/bin"
-
-rm -rf $GOPATH
-mkdir $GOPATH
-
-PROJECT_PATH="github.com/alphagov"
-PROJECT_NAME="router"
-mkdir -p ${GOPATH}/src/${PROJECT_PATH}
-ln -s ../../../.. ${GOPATH}/src/${PROJECT_PATH}/${PROJECT_NAME}
-
-go get -v -d
-go build -v -o ${PROJECT_NAME}
-
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
+
+./compile.sh
+
 bundle exec fpm \
   -s dir -t deb \
   -n ${PROJECT_NAME} -v ${BUILD_NUMBER} \
