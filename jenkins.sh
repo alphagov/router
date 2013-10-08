31bcb72a8e5da9d8eb6cdbda70283431917fe993
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-10-08 13:22:20 +0000

    Make Jenkins run Go unit tests

--- jenkins.sh
+++ jenkins.sh
@@ -8,6 +8,8 @@ bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
 source ./build_gopath.sh
 go build -v -o ${PROJECT_NAME}
+
+go test ./triemux
 USE_COMPILED_ROUTER=1 bundle exec rspec
 
 bundle exec fpm \
