00dfc5757577aa8f868fca3a1155dc85c2bfba24
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-10-08 15:07:13 +0000

    Add echo test backend.
    This returns a JSON body containing the received request details, and
    the request body (the http.Request object contains the Body as an
    io.Reader, which doesn't serialise into JSON easily...)

---
+++ spec/test_backends/echo_backend.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var port = flag.Int("port", 3160, "The port to listen on")
+
+func echoResponse(w http.ResponseWriter, r *http.Request) {
+	data := make(map[string]interface{})
+	data["Request"] = r
+
+	body, _ := ioutil.ReadAll(r.Body)
+	data["Body"] = string(body)
+
+	json_data, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	json_data = append(json_data, 10) // Add final newline
+	w.Write(json_data)
+}
+
+func main() {
+	flag.Parse()
+
+	http.HandleFunc("/", echoResponse)
+
+	addr := fmt.Sprintf(":%d", *port)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
