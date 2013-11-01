3293c3000a637833c40511e26619276a8ab1da35
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-11-01 09:47:33 +0000

    Refactor tarpit backend to not use ServeMux
    Makes it consistent with the other backends.

--- spec/test_backends/tarpit_backend.go
+++ spec/test_backends/tarpit_backend.go
@@ -10,31 +10,31 @@ import (
 )
 
 var port = flag.Int("port", 3160, "The port to listen on")
-var responseDelay = flag.Duration("response-delay", 2 * time.Second, "Delay in seconds before start of response")
+var responseDelay = flag.Duration("response-delay", 2*time.Second, "Delay in seconds before start of response")
 var bodyDelay = flag.Duration("body-delay", 0, "Delay in seconds before start of response")
+
+func tarpitResponder(w http.ResponseWriter, r *http.Request) {
+	body := "Tarpit\n"
+
+	if *responseDelay > 0 {
+		time.Sleep(*responseDelay)
+	}
+	w.Header().Add("Content-Length", strconv.Itoa(len(body)))
+	w.WriteHeader(http.StatusOK)
+	w.(http.Flusher).Flush()
+
+	if *bodyDelay > 0 {
+		time.Sleep(*bodyDelay)
+	}
+	w.Write([]byte(body))
+}
 
 func main() {
 	flag.Parse()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		body := "Tarpit\n"
-
-		if *responseDelay > 0 {
-			time.Sleep(*responseDelay)
-		}
-		w.Header().Add("Content-Length", strconv.Itoa(len(body)))
-		w.WriteHeader(http.StatusOK)
-		w.(http.Flusher).Flush()
-
-		if *bodyDelay > 0 {
-			time.Sleep(*bodyDelay)
-		}
-		w.Write([]byte(body))
-	})
-
 	addr := fmt.Sprintf(":%d", *port)
 
-	err := http.ListenAndServe(addr, nil)
+	err := http.ListenAndServe(addr, http.HandlerFunc(tarpitResponder))
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
