bcfa626f132dd513adfd432ac1d0036bf9600f5e
Dan Carley <dan.carley@gmail.com>
2013-11-04 11:26:00 +0000

    [#60058920] Actual tests for perf regressions
    To test the three areas that we've currently identified:
    
    - Two healthy backends.
    - Ditto, and one slow backend.
    - Ditto, and one missing backend.
    
    Implementation of Vegeta has been moved to a helper. Currently uses the
    default rate of 50rps and duration of 10s. Tests take a total of 60s.

--- spec/performance_spec.rb
+++ spec/performance_spec.rb
@@ -1,57 +1,97 @@
 require 'spec_helper'
-require 'open3'
-require 'pp'
 
-def vegeta(requests)
-  rate = 1000
-  puts "Rate: #{rate} rps"
-  cmd = "./vegeta attack -rate #{rate}"
-  Open3.popen2(cmd) {|i,o,t|
-    i.puts requests
-    i.close
-    Open3.popen2("./vegeta report -reporter json") {|i2,o2,t2|
-      i2.puts o.read
-      i2.close
-      return JSON.parse(o2.read)
-    }
-  }
-end
+describe "performance" do
+  describe "two healthy backends" do
+    start_backend_around_all :port => 3160, :identifier => "backend 1"
+    start_backend_around_all :port => 3161, :identifier => "backend 2"
 
-describe "perf" do
-
-  start_backend_around_all :port => 3160, :identifier => "backend 1"
-  start_backend_around_all :port => 3161, :identifier => "backend 2"
-
-  before :each do
-    add_backend("backend-1", "http://localhost:3160/")
-    add_backend("backend-2", "http://localhost:3161/")
-    add_backend_route("/one", "backend-1")
-    add_backend_route("/two", "backend-2")
-    reload_routes
-  end
-
-  it "should be fast as LIGHTNING" do
-    direct = vegeta <<EOF
-GET http://localhost:3160/one
-GET http://localhost:3161/two
-EOF
-    router = vegeta <<EOF
-GET #{router_url("/one")}
-GET #{router_url("/two")}
-EOF
-
-    results = {}
-    direct['latencies'].each_key do |key|
-      d = direct['latencies'][key].to_f
-      r = router['latencies'][key].to_f
-      results[key] = {
-        :direct => "%.2f us" % [d / 1000],
-        :router => "%.2f us" % [r / 1000],
-        :difference => "%.2f us" % [(r - d) / 1000],
-        :percentage => "%d%%" % [(r / d) * 100],
-      }
+    before :each do
+      add_backend("backend-1", "http://localhost:3160/")
+      add_backend("backend-2", "http://localhost:3161/")
+      add_backend_route("/one", "backend-1")
+      add_backend_route("/two", "backend-2")
+      reload_routes
     end
 
-    pp results
+    it "should not increase latency by more than twofold" do
+      results_direct = vegeta_request_latency(["http://localhost:3160/one", "http://localhost:3161/two"])
+      results_router = vegeta_request_latency([router_url("/one"), router_url("/two")])
+
+      expect(results_router["max"]).to  be_within(200).percent_of(results_direct["max"])
+      expect(results_router["mean"]).to be_within(200).percent_of(results_direct["mean"])
+      expect(results_router["95th"]).to be_within(200).percent_of(results_direct["95th"])
+      expect(results_router["99th"]).to be_within(200).percent_of(results_direct["99th"])
+    end
+  end
+
+  describe "one slow backend" do
+    start_backend_around_all :port => 3160, :identifier => "backend 1"
+    start_backend_around_all :port => 3161, :identifier => "backend 2"
+    start_backend_around_all :port => 3162, :type => :tarpit, "response-delay" => "1s"
+
+    before :each do
+      add_backend("backend-1", "http://localhost:3160/")
+      add_backend("backend-2", "http://localhost:3161/")
+      add_backend("backend-slow", "http://localhost:3162/")
+      add_backend_route("/one", "backend-1")
+      add_backend_route("/two", "backend-2")
+      add_backend_route("/slow", "backend-slow")
+      reload_routes
+    end
+
+    start_vegeta_load_around_all("/slow")
+
+    it "should not impact other backends" do
+      opts = {:duration => "11s"}
+
+      results_direct = vegeta_request_latency(
+        ["http://localhost:3160/one", "http://localhost:3161/two"],
+        opts
+      )
+      results_router = vegeta_request_latency(
+        [router_url("/one"), router_url("/two")],
+        opts
+      )
+
+      expect(results_router["max"]).to  be_within(200).percent_of(results_direct["max"])
+      expect(results_router["mean"]).to be_within(200).percent_of(results_direct["mean"])
+      expect(results_router["95th"]).to be_within(200).percent_of(results_direct["95th"])
+      expect(results_router["99th"]).to be_within(200).percent_of(results_direct["99th"])
+    end
+  end
+
+  describe "one downed backend" do
+    start_backend_around_all :port => 3160, :identifier => "backend 1"
+    start_backend_around_all :port => 3161, :identifier => "backend 2"
+
+    before :each do
+      add_backend("backend-1", "http://localhost:3160/")
+      add_backend("backend-2", "http://localhost:3161/")
+      add_backend("backend-down", "http://localhost:3162/")
+      add_backend_route("/one", "backend-1")
+      add_backend_route("/two", "backend-2")
+      add_backend_route("/down", "backend-down")
+      reload_routes
+    end
+
+    start_vegeta_load_around_all("/down")
+
+    it "should not be impacted by a missing backend" do
+      opts = {:duration => "11s"}
+
+      results_direct = vegeta_request_latency(
+        ["http://localhost:3160/one", "http://localhost:3161/two"],
+        opts
+      )
+      results_router = vegeta_request_latency(
+        [router_url("/one"), router_url("/two")],
+        opts
+      )
+
+      expect(results_router["max"]).to  be_within(200).percent_of(results_direct["max"])
+      expect(results_router["mean"]).to be_within(200).percent_of(results_direct["mean"])
+      expect(results_router["95th"]).to be_within(200).percent_of(results_direct["95th"])
+      expect(results_router["99th"]).to be_within(200).percent_of(results_direct["99th"])
+    end
   end
 end
