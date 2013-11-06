f65921d88f369bb2591d0d5e8de1a7f202260793
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-11-06 11:10:15 +0000

    Vendor the mongo driver.

--- build_gopath.sh
+++ build_gopath.sh
@@ -6,13 +6,14 @@ REPO_ROOT=$(dirname ${BASH_SOURCE[0]})
 cd $REPO_ROOT
 REPO_ROOT=$(pwd) # Resolve relative paths
 
-export GOPATH="$REPO_ROOT/gopath.tmp"
-export GOBIN="$GOPATH/bin"
+GOPATH_TMP="${REPO_ROOT}/gopath.tmp"
+export GOPATH="${GOPATH_TMP}:${REPO_ROOT}/vendor"
+export GOBIN="${GOPATH_TMP}/bin"
 
 PROJECT_PATH="github.com/alphagov/router"
-mkdir -p $(dirname ${GOPATH}/src/${PROJECT_PATH})
+mkdir -p $(dirname ${GOPATH_TMP}/src/${PROJECT_PATH})
 
-rm -f ${GOPATH}/src/${PROJECT_PATH}
-ln -s ../../../.. ${GOPATH}/src/${PROJECT_PATH}
+rm -f ${GOPATH_TMP}/src/${PROJECT_PATH}
+ln -s ../../../.. ${GOPATH_TMP}/src/${PROJECT_PATH}
 
 go get -v -d
