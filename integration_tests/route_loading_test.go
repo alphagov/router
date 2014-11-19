b44339586ff7e81cc4db2ed3b1844ec8ebf91dec
Alex Tomlins <alex@tomlins.org.uk>
2014-11-19 12:34:41 +0000

    Port route loading tests to Go

---
+++ integration_tests/route_loading_test.go
@@ -0,0 +1,74 @@
+package integration
+
+import (
+	"net/http/httptest"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("loading routes from the db", func() {
+	var (
+		backend1 *httptest.Server
+		backend2 *httptest.Server
+	)
+
+	BeforeEach(func() {
+		backend1 = startSimpleBackend("backend 1")
+		backend2 = startSimpleBackend("backend 2")
+		addBackend("backend-1", backend1.URL)
+		addBackend("backend-2", backend2.URL)
+	})
+	AfterEach(func() {
+		backend1.Close()
+		backend2.Close()
+	})
+
+	Context("a route with an unrecognised handler type", func() {
+		BeforeEach(func() {
+			addBackendRoute("/foo", "backend-1")
+			addRoute("/bar", map[string]interface{}{"handler": "fooey"})
+			addBackendRoute("/baz", "backend-2")
+			reloadRoutes()
+		})
+
+		It("should skip the invalid route", func() {
+			resp := routerRequest("/bar")
+			Expect(resp.StatusCode).To(Equal(404))
+		})
+
+		It("should continue to load other routes", func() {
+			resp := routerRequest("/foo")
+			Expect(readBody(resp)).To(Equal("backend 1"))
+
+			resp = routerRequest("/baz")
+			Expect(readBody(resp)).To(Equal("backend 2"))
+		})
+	})
+
+	Context("a route with a non-existent backend", func() {
+		BeforeEach(func() {
+			addBackendRoute("/foo", "backend-1")
+			addBackendRoute("/bar", "backend-non-existent")
+			addBackendRoute("/baz", "backend-2")
+			addBackendRoute("/qux", "backend-1")
+			reloadRoutes()
+		})
+
+		It("should skip the invalid route", func() {
+			resp := routerRequest("/bar")
+			Expect(resp.StatusCode).To(Equal(404))
+		})
+
+		It("should continue to load other routes", func() {
+			resp := routerRequest("/foo")
+			Expect(readBody(resp)).To(Equal("backend 1"))
+
+			resp = routerRequest("/baz")
+			Expect(readBody(resp)).To(Equal("backend 2"))
+
+			resp = routerRequest("/qux")
+			Expect(readBody(resp)).To(Equal("backend 1"))
+		})
+	})
+})
