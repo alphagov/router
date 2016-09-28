987b52b91900bc6cda71ee1d20d1d487bed723f0
Tim Blair <tim@bla.ir>
2016-09-28 09:26:08 +0100

    Clarify the CI build process
    The CI build process for Go apps is slightly "different," in that the
    app needs to be built from within a Go workspace.  This change simply
    comments and clarifies what's going on and why.
    
    Functionally, there's nothing new happening in this change, other than
    making use of the ability to pass through a binary location to `make`,
    rather than copying it out after the build.

--- jenkins.sh
+++ jenkins.sh
@@ -2,17 +2,35 @@
 set -x
 set -eu
 
-REPO=alphagov/router
-export GOPATH=$PWD/gopath
-GO_GITHUB_PATH=$GOPATH/src/github.com
-BUILD_PATH=$GO_GITHUB_PATH/$REPO
+# The repository owner and name
+REPO="alphagov/router"
+# The name of the binary to output
+BINARY=$(basename "$REPO")
 
-rm -rf $GOPATH && mkdir -p $GOPATH/bin $BUILD_PATH
+# Define the Jenkins workspace root in case we're not running through Jenkins
+: ${WORKSPACE:=$PWD}
 
-rsync -a ./ $BUILD_PATH --exclude=gopath
+# Go projects need to be built from with a Go workspace, which is a directory
+# tree with a specific format.  The default checkout path isn't enough, so we
+# need to create that workspace and build from within there.  The GOPATH
+# envionment variable points to the location of the workspace.
+#
+# See https://golang.org/doc/code.html#Workspaces for more details.
+BUILD_DIR="__build"
+export GOPATH="$WORKSPACE/$BUILD_DIR"
 
-cd $BUILD_PATH && make
-cp ./router $WORKSPACE/router
+# Define the location within the GOPATH that the source should reside
+SRC_PATH="$GOPATH/src/github.com/$REPO"
 
-make test
-./router -version
+# Recreate the GOPATH workspace from scratch
+rm -rf "$GOPATH" && mkdir -p "$GOPATH/bin" "$SRC_PATH"
+
+# Copy the whole repo content into the build tree
+rsync -a ./ "$SRC_PATH" --exclude="$BUILD_DIR"
+
+# Move in to the source path, then build the binary into the Jenkins workspace
+# root (for later packaging) and run the tests
+(cd "$SRC_PATH" && BINARY="$WORKSPACE/$BINARY" make clean build test)
+
+# Output the version that was built
+"./$BINARY" -version
