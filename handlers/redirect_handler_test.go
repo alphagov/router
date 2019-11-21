27f52ddd3096a0bc285825fc1b65a33749034730
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-11-21 16:07:10 +0000

    Add some tests for the redirect handlers
    This describes the current behaviour in a BDD format
    
    So that future contributors can take the same cosmic journey that I have
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ handlers/redirect_handler_test.go
@@ -0,0 +1,160 @@
+package handlers_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	"github.com/alphagov/router/handlers"
+)
+
+type redirectTableEntry struct {
+	preserve  bool
+	temporary bool
+}
+
+var _ = Describe("Redirect handlers", func() {
+	entries := []TableEntry{
+		Entry(
+			"when redirects are temporary and paths are preserved",
+			redirectTableEntry{preserve: true, temporary: true},
+		),
+		Entry(
+			"when redirects are temporary and paths are not preserved",
+			redirectTableEntry{preserve: false, temporary: true},
+		),
+		Entry(
+			"when redirects are not temporary and paths are preserved",
+			redirectTableEntry{preserve: true, temporary: false},
+		),
+		Entry(
+			"when redirects are not temporary and paths are not preserved",
+			redirectTableEntry{preserve: false, temporary: false},
+		),
+	}
+
+	DescribeTable(
+		"handlers",
+		func(t redirectTableEntry) {
+			rw := httptest.NewRecorder()
+
+			handler := handlers.NewRedirectHandler(
+				"/source-prefix", "/target-prefix",
+				t.preserve, t.temporary,
+			)
+
+			handler.ServeHTTP(
+				rw,
+				httptest.NewRequest(
+					"GET",
+					"https://source.gov.uk/source-prefix/path/subpath?query1=a&query2=b",
+					nil,
+				),
+			)
+
+			if t.temporary {
+				// HTTP 302 is returned instead of 307
+				// because we want the route to be cached temporarily
+				// and not rerequested immediately
+				Expect(rw.Result().StatusCode).To(
+					Equal(http.StatusFound),
+					"when the redirect is temporary we should return HTTP 302",
+				)
+			} else {
+				Expect(rw.Result().StatusCode).To(
+					Equal(http.StatusMovedPermanently),
+					"when the redirect is permanent we should return HTTP 301",
+				)
+			}
+
+			if t.preserve {
+				Expect(rw.Result().Header.Get("Location")).To(
+					Equal("/target-prefix/path/subpath?query1=a&query2=b"),
+				)
+			} else {
+				Expect(rw.Result().Header.Get("Location")).To(
+					Equal("/target-prefix"),
+					"when we do not preserve the path, we redirect straight to target",
+				)
+			}
+
+			Expect(rw.Result().Header.Get("Cache-Control")).To(
+				SatisfyAll(
+					ContainSubstring("public"),
+					ContainSubstring("max-age=1800"),
+				),
+				"Declare public and cachable for 30 minutes",
+			)
+
+			Expect(rw.Result().Header.Get("Expires")).To(
+				WithTransform(
+					func(timestr string) time.Time {
+						t, err := time.Parse(time.RFC1123, timestr)
+						Expect(err).NotTo(HaveOccurred(), "Not RFC1123 compliant")
+						return t
+					},
+					BeTemporally("~", time.Now().Add(30*time.Minute), 1*time.Second),
+				),
+				"Be RFC1123 compliant and expire around 30 minutes in the future",
+			)
+		},
+		entries...,
+	)
+
+	Context("when we are not preserving paths", func() {
+		var (
+			rw      *httptest.ResponseRecorder
+			handler http.Handler
+		)
+
+		BeforeEach(func() {
+			rw = httptest.NewRecorder()
+
+			handler = handlers.NewRedirectHandler(
+				"/source-prefix", "/target-prefix",
+				false, // preserve
+				true,  // temporary
+			)
+		})
+
+		Context("when the _ga query param is present", func() {
+			It("should persist _ga to the query params", func() {
+				handler.ServeHTTP(
+					rw,
+					httptest.NewRequest(
+						"GET",
+						"https://source.gov.uk/source-prefix?_ga=dontbeevil",
+						nil,
+					),
+				)
+
+				Expect(rw.Result().Header.Get("Location")).To(
+					Equal("/target-prefix?_ga=dontbeevil"),
+					"Preserve the _ga query parameter",
+				)
+			})
+		})
+
+		Context("when the _ga query param is not present", func() {
+			It("should not add _ga to the query params", func() {
+				handler.ServeHTTP(
+					rw,
+					httptest.NewRequest(
+						"GET",
+						"https://source.gov.uk/source-prefix?param=begood",
+						nil,
+					),
+				)
+
+				Expect(rw.Result().Header.Get("Location")).To(
+					Equal("/target-prefix"),
+					"Do not have any query params",
+				)
+			})
+		})
+	})
+})
