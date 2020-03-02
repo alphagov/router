32e5de053a6177f18824a3a99fcfa2e6b8094e12
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-03-02 09:38:16 +0000

    Add backend handler metric req count / resp secs
    We want to measure:
    * the number of requests
    * the duration of responses
    
    We measure the backend handlers individually, so the metrics are defined
    as prometheus vectors
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- handlers/metrics.go
+++ handlers/metrics.go
@@ -16,8 +16,32 @@ var (
 			"redirect_url",
 		},
 	)
+
+	BackendHandlerRequestCountMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "router_backend_handler_request_count",
+			Help: "Number of requests handled by router backend handlers",
+		},
+		[]string{
+			"backend_id",
+		},
+	)
+
+	BackendHandlerResponseDurationSecondsMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "router_backend_handler_response_duration_seconds",
+			Help: "Time in seconds spent proxying requests to backends by router backend handlers",
+		},
+		[]string{
+			"backend_id",
+			"response_code",
+		},
+	)
 )
 
 func initMetrics() {
 	prometheus.MustRegister(RedirectHandlerRedirectCountMetric)
+
+	prometheus.MustRegister(BackendHandlerRequestCountMetric)
+	prometheus.MustRegister(BackendHandlerResponseDurationSecondsMetric)
 }
