a4b6fed4616b19e4882c213a0504f9d0e69cf8d5
Kelvin Gan <dev@kelvingan.co.uk>
2022-10-18 14:22:30 +0100

    Revert "Bump github.com/onsi/gomega from 1.21.1 to 1.22.1"

--- vendor/github.com/onsi/gomega/types/types.go
+++ vendor/github.com/onsi/gomega/types/types.go
@@ -74,7 +74,6 @@ type AsyncAssertion interface {
 	Within(timeout time.Duration) AsyncAssertion
 	ProbeEvery(interval time.Duration) AsyncAssertion
 	WithContext(ctx context.Context) AsyncAssertion
-	WithArguments(argsToForward ...interface{}) AsyncAssertion
 }
 
 // Assertions are returned by Ω and Expect and enable assertions against Gomega matchers
