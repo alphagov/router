05e2e57f0f4e8f14dd81d77ad9e371c58d671213
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-07-28 15:26:18 +0100

    Remove inappropriate integration tests for metrics.
    We shouldn't be testing the Prometheus library, just our own code. See
    https://pkg.go.dev/github.com/prometheus/client_golang/prometheus/testutil
    
    Keep one integration test to check that we're actually serving
    `/metrics`, but delete the rest. Everything else can be done better with
    unit tests.

--- integration_tests/metrics_test.go
+++ integration_tests/metrics_test.go
@@ -5,16 +5,9 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("metrics API endpoint", func() {
-	It("should return HTTP OK when receiving a request for /metrics", func() {
-		resp := doRequest(newRequest("GET", routerAPIURL("/metrics")))
-		Expect(resp.StatusCode).To(Equal(200))
-	})
-
-	Context("when looking at metrics returned in response", func() {
-		var (
-			responseBody string
-		)
+var _ = Describe("/metrics API endpoint", func() {
+	Context("response body", func() {
+		var responseBody string
 
 		BeforeEach(func() {
 			resp := doRequest(newRequest("GET", routerAPIURL("/metrics")))
@@ -22,16 +15,8 @@ var _ = Describe("metrics API endpoint", func() {
 			responseBody = readBody(resp)
 		})
 
-		It("should contain router internal server error metrics", func() {
-			Skip("Metric is a vector so it is not registered by default")
-			Expect(responseBody).To(ContainSubstring("router_internal_server_error_total"))
-		})
-
-		It("should contain routing table metrics", func() {
-			Expect(responseBody).To(ContainSubstring("router_route_reload_total"))
-			Expect(responseBody).To(ContainSubstring("router_route_reload_error_total"))
-
-			Expect(responseBody).To(ContainSubstring("router_routes_loaded"))
+		It("should contain at least one metric", func() {
+			Expect(responseBody).To(ContainSubstring("router_"))
 		})
 	})
 })
