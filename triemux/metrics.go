18270cd21280d82d5dedd0bdae6cdf308483c575
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-08-05 16:45:31 +0100

    Remove a Tablecloth reference missed in acfdcc7.

--- triemux/metrics.go
+++ triemux/metrics.go
@@ -12,12 +12,11 @@ var (
 		},
 	)
 
-	internalServiceUnavailableCountMetric = prometheus.NewCounterVec(
+	internalServiceUnavailableCountMetric = prometheus.NewCounter(
 		prometheus.CounterOpts{
 			Name: "router_service_unavailable_error_total",
 			Help: "Number of 503 Service Unavailable errors served by router",
 		},
-		[]string{"temporary_child"},
 	)
 )
 
