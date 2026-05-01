2eb15b469393a605c58c331bd8cbf510d9500162
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2026-05-01 11:04:13 +0100

    Add step to PR workflow to post coverage on comment in a PR

--- coverage/generate-markdown-summary.sh
+++ coverage/generate-markdown-summary.sh
@@ -5,31 +5,36 @@ set -euo pipefail
 RAW_REPORT=$(mktemp)
 COVERAGE_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-go tool covdata percent -i "${COVERAGE_DIR}/merged" > "$RAW_REPORT"
-
-MARKDOWN_REPORT="${COVERAGE_DIR}/report/overall-coverage.md"
-cat > "$MARKDOWN_REPORT" <<EOF
-## Coverage report
-
-Module | Statements Covered | Happy?
--------|:------------------:|:------:
-EOF
-
 function emoji_for_percent {
   local PERCENT=$1
 
   INT_VALUE=$(printf "%.0f" "$PERCENT")
 
-  if [ "$INT_VALUE" -ge 90 ]; then
-    echo ":heart_eyes:"
+  if [ "$INT_VALUE" -ge 95 ]; then
+    echo ":star:"
+  elif [ "$INT_VALUE" -ge 90 ]; then
+    echo ":white_tick:"
   elif [ "$INT_VALUE" -ge 80 ]; then
-    echo ":smile:"
-  elif [ "$INT_VALUE" -ge 70 ]; then
-    echo ":sweat:"
+    echo ":red_circle:"
   else
-    echo ":fearful:"
+    echo ":broken_heart:"
   fi
 }
+
+go tool covdata percent -i "${COVERAGE_DIR}/merged" > "$RAW_REPORT"
+
+MARKDOWN_REPORT="${COVERAGE_DIR}/report/overall-coverage.md"
+OVERALL_COVERAGE=$(awk '{ print substr($3, 1, length($3)-1); }' "${COVERAGE_DIR}/report/overall-coverage.txt")
+OVERALL_EMOJI=$(emoji_for_percent "$OVERALL_COVERAGE")
+
+cat > "$MARKDOWN_REPORT" <<EOF
+## Coverage report
+
+Overall coverage: ${OVERALL_COVERAGE}% ${OVERALL_EMOJI}
+
+Package | Statements Covered | Happy?
+-------|:------------------:|:------:
+EOF
 
 while read -r LINE; do
   PACKAGE=$(awk '{ print $1; '} <<<"$LINE")
@@ -38,8 +43,3 @@ while read -r LINE; do
 
   echo "${PACKAGE} | ${PERCENTAGE}% | $EMOJI" >> "$MARKDOWN_REPORT"
 done < "$RAW_REPORT"
-
-OVERALL_COVERAGE=$(awk '{ print substr($3, 1, length($3)-1); }' <"${COVERAGE_DIR}/report/overall-coverage.txt")
-OVERALL_EMOJI=$(emoji_for_percent "$OVERALL_COVERAGE")
-
-echo -e "\n\nOverall coverage: ${OVERALL_COVERAGE}% ${OVERALL_EMOJI}\n" >> "$MARKDOWN_REPORT"
